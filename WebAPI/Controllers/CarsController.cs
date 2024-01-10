aa39866ad8f249fb18802898c5ec012c68ea8b0a
Metehan Bilen <90154967+MetehanBilen@users.noreply.github.com>
2024-01-10 19:25:47 +0300

    Add files via upload

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,64 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.Concrete;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult Get()
+        {
+            var result = _carService.GetAll();
+            if (result.Success)
+            { return Ok(result); }
+
+            return BadRequest(result);
+        }
+
+        [HttpGet("getalldetails")]
+        public IActionResult GetCarDetails()
+        {
+            var result = _carService.GetCarDetails();
+            if (result.Success)
+            { return Ok(result); }
+
+            return BadRequest(result);
+        }
+
+
+
+        [HttpPost("add")]
+        public IActionResult Add(Car car)
+        {
+            var result = _carService.Add(car);
+            if (result.Success) { return Ok( result); }
+            return BadRequest(result);
+
+        }
+
+        [HttpGet("getbycarid")]
+        public IActionResult GetByCarId(int carId)
+        {
+            var result = _carService.GetByCarId(carId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+}
---
+++ WebAPI/Controllers/ColorsController.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.Concrete;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColorsController : ControllerBase
+    {
+        IColorService _colorService;
+
+        public ColorsController(IColorService colorService)
+        {
+            _colorService = colorService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult Get()
+        {
+            var result = _colorService.GetAll();
+            if (result.Success)
+            { return Ok(result); }
+
+            return BadRequest(result);
+        }
+
+
+    }
+}
